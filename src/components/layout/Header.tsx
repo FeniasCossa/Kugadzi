9dfd2162d720fa0319574428c8484d8c1bfdb046
Fenias Cossa <fenisasebastiao636@gmail.com>
2025-10-27 15:08:46 +0200

    fix[01.00.00]: Header setion

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Link as RouterLink, useLocation } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import QuoteModal from './QuoteModal';
 
